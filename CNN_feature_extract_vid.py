41b55bbbe559d692d442b3c3ed866db8f9c678ad
Moin Uddin <moin@wearhealth.com>
2020-02-07 17:20:37 +0100

    upload: python script for feature extraction

---
+++ CNN_feature_extract_vid.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import cv2
+import numpy as np
+caffe_root = '/home/wh/caffe/python'
+sys.path.insert(0, os.path.join(caffe_root, 'caffe'))
+import caffe
+caffe.set_mode_gpu()
+
+####class 1 for stand & class 0 for sitting
+cap = cv2.VideoCapture(0)
+
+while cv2.waitKey(1) < 0:
+    ret,input_image_file = cap.read()
+    ##### feature extract by caffenet
+    model_file = 'bvlc_reference_caffenet.caffemodel'
+    deploy_prototxt = 'deploy.prototxt'
+
+    ### feature extract by VGG-19
+    # model_file = '/home/wh/Thesis/feature_extract/vgg/VGG_ILSVRC_19_layers.caffemodel'
+    # deploy_prototxt = '/home/wh/Thesis/feature_extract/vgg/VGG_ILSVRC_19_layers_deploy.prototxt'
+
+    convNet = caffe.net(deploy_prototxt, model_file, caffe.TEST)
+    layer = 'fc6'
+    if layer not in convNet.blobs:
+        raise TypeError("Invalid layer name: " + layer)
+
+    imagemean_file = 'ilsvrc_2012_mean.npy'
+
+    transformer = caffe.io.Transformer({'data': convNet.blobs['data'].data.shape})
+    transformer.set_mean('data', np.load(imagemean_file).mean(1).mean(1))
+    transformer.set_transpose('data', (2,0,1))
+    transformer.set_raw_scale('data', 255.0)
+
+    convNet.blobs['data'].reshape(1,3,227,227)
+    # img = caffe.io.load_image(input_image_file)
+    img =np.copy(input_image_file)
+    convNet.blobs['data'].data[...] = transformer.preprocess('data', img)
+
+    output = convNet.forward()
+
+    # generated key-points value save into a txt file
+    with open("feature_stand.txt", 'a') as file:
+        abstract = convNet.blobs[layer].data[0]
+        new_list = []
+        for item in abstract:
+            new_list.append(float(item))
+        file.write(str(new_list) + '\n'+'"Standing",')
+        print(new_list)
+            # coordinate_value = np.array(abstract, dtype=np.float32)
+    # with open('output_file.txt', 'w') as f:
+    #     np.savetxt(f, convNet.blobs[layer].data[0], fmt='%.4f', delimiter='\n')
